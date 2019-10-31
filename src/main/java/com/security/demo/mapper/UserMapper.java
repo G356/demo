6c21392c44e91c26e5b9db9db5e9afd091c5c08a
tom_glb <goodnight_glb@guigu.com>
2019-11-01 00:08:32 +0900

    security Initial commit

---
+++ src/main/java/com/security/demo/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.security.demo.mapper;
+import com.security.demo.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface UserMapper {
+
+    @Select( "select id , username , password from user where username = #{username}" )
+    User loadUserByUsername(@Param("username") String username);
+    @Update("update user set password = #{password} where username=#{username}")
+    int update(User user);
+}
---
+++ src/test/java/com/security/demo/DemoApplicationTests.java
@@ -0,0 +1,30 @@
+package com.security.demo;
+
+import com.security.demo.entity.User;
+import com.security.demo.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+class DemoApplicationTests {
+@Autowired
+    UserMapper userMapper;
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void update() {
+        User user = new User();
+        user.setUsername("user");
+        String password="123456";
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashPass=passwordEncoder.encode(password);
+        System.out.println("密码一致"+passwordEncoder.matches(password, hashPass));
+        user.setPassword(hashPass);
+        userMapper.update(user);
+    }
+
+}
